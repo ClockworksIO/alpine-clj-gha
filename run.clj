01a85f768d6da42f19ee15d83dbabdc9edfb9725
Moritz Moxter <moritz@clockworks.io>
2023-01-27 13:18:25 +0100

    chore: try fix output values

--- run.clj
+++ run.clj
@@ -4,6 +4,8 @@
     [babashka.process :refer [sh]]
     [clojure.string :as str]))
 
+(def GH-OUTPUT (System/getenv "GITHUB_OUTPUT"))
+
 (def clj-path "/usr/local/bin/clojure")
 (def clj-version (-> (sh "clojure --version") :out (str/split #" ") last))
 
@@ -12,8 +14,14 @@
 
 (def java-version (-> (sh "java -version") :out str/split-lines first))
 
-(sh (format "echo \"java_version=%s\" >> $GITHUB_OUTPUT" java-version))
-(sh (format "echo \"clojure_path=%s\" >> $GITHUB_OUTPUT" clj-path))
-(sh (format "echo \"clojure_version=%s\" >> $GITHUB_OUTPUT" clj-version))
-(sh (format "echo \"babashka_path=%s\" >> $GITHUB_OUTPUT" babashka-path))
-(sh (format "echo \"babashka_version=%s\" >> $GITHUB_OUTPUT" babashka-version))
+(spit GH-OUTPUT (format "java_version=%s/n" java-version) :append true)
+(spit GH-OUTPUT (format "clojure_path=%s/n" clj-path) :append true)
+(spit GH-OUTPUT (format "clojure_version=%s/n" clojure-version) :append true)
+(spit GH-OUTPUT (format "babashka_path=%s/n" babashka-path) :append true)
+(spit GH-OUTPUT (format "babashka_version=%s/n" babashka-version) :append true)
+
+; (sh (format "echo \"java_version=%s\" >> $GITHUB_OUTPUT" java-version))
+; (sh (format "echo \"clojure_path=%s\" >> $GITHUB_OUTPUT" clj-path))
+; (sh (format "echo \"clojure_version=%s\" >> $GITHUB_OUTPUT" clj-version))
+; (sh (format "echo \"babashka_path=%s\" >> $GITHUB_OUTPUT" babashka-path))
+; (sh (format "echo \"babashka_version=%s\" >> $GITHUB_OUTPUT" babashka-version))
